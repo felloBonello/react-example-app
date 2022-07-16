f5d56ba9e7e15dfb59b5a62658d6d5ee2b8dd6ac
felloBonello <justin.w.bonello@gmail.com>
2022-07-16 17:37:48 -0400

    add items to list

--- src/app/TodoListComponent.js
+++ src/app/TodoListComponent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 export class TodoListComponent extends React.Component {
+
+    constructor({todos}) {
+        super();
+        this.todos = todos;
+    }
+
     render() {
-        return <ul data-testid='todo-list-component'></ul>;
+        return <ul data-testid='todo-list-component'>
+            {
+                this.todos.map(item => <li key={item.id} data-testid={`todo-${item.id}`}>{ item.text }</li> )
+            }
+        </ul>;
     }
 }
--- src/app/TodoListComponent.spec.js
+++ src/app/TodoListComponent.spec.js
@@ -7,4 +7,23 @@ describe("TodoListComponent", () => {
         // eslint-disable-next-line testing-library/prefer-presence-queries
         expect(screen.queryByTestId('todo-list-component')).toBeTruthy();
     });
+
+    it("will render out todos", () => {
+        const todos = [
+            {
+                id: 1,
+                text: "I am a todo"
+            },
+            {
+                id: 2,
+                text: "I am the second todo"
+            }
+        ];
+        render(<TodoListComponent todos={todos}/>);
+
+        todos.forEach((item) => {
+            // eslint-disable-next-line testing-library/prefer-presence-queries
+            expect(screen.queryByTestId(`todo-${item.id}`)).toBeTruthy();
+        })
+    });
 });
