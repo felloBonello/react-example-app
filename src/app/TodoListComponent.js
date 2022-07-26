6002966b3846175572c60331e480a7d73ed23f77
Nathan Davis <nathan.davis.ext@siemens..com>
2022-07-26 15:58:03 -0400

    green tests

--- src/app/TodoListComponent.js
+++ src/app/TodoListComponent.js
@@ -1,17 +1,24 @@
 import React from "react";
 
 export class TodoListComponent extends React.Component {
+  static defaultProps = {
+    todos: [],
+  };
 
-    constructor({todos}) {
-        super();
-        this.todos = todos;
-    }
+  constructor({ todos }) {
+    super();
+    this.todos = todos;
+  }
 
-    render() {
-        return <ul data-testid='todo-list-component'>
-            {
-                this.todos.map(item => <li key={item.id} data-testid={`todo-${item.id}`}>{ item.text }</li> )
-            }
-        </ul>;
-    }
+  render() {
+    return (
+      <ul data-testid="todo-list-component">
+        {this.todos.map((item) => (
+          <li key={item.id} data-testid={`todo-${item.id}`}>
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
