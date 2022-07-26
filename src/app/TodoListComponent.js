ec0a853b19db3b1e792f48b264beca6935db1570
Nathan Davis <nathan.davis.ext@siemens..com>
2022-07-26 17:00:33 -0400

    redux store

--- src/app/TodoListComponent.js
+++ src/app/TodoListComponent.js
@@ -1,24 +1,31 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { AddTodo } from "./AddTodo";
 export class TodoListComponent extends React.Component {
-  static defaultProps = {
-    todos: [],
-  };
-
-  constructor({ todos }) {
-    super();
-    this.todos = todos;
-  }
-
   render() {
+    const { todos } = this.props;
+
     return (
-      <ul data-testid="todo-list-component">
-        {this.todos.map((item) => (
-          <li key={item.id} data-testid={`todo-${item.id}`}>
-            {item.text}
-          </li>
-        ))}
-      </ul>
+      <>
+        <AddTodo callback={(action) => this.props.dispatch(action)} />
+        <ul data-testid="todo-list-component">
+          {todos.map((item) => (
+            <li key={item.id} data-testid={`todo-${item.id}`}>
+              {item.text}
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const todo = state.todo;
+
+  return {
+    todos: todo.todos,
+  };
+}
+
+export default connect(mapStateToProps)(TodoListComponent);
--- src/app/TodoListComponent.spec.js
+++ src/app/TodoListComponent.spec.js
@@ -1,29 +1,30 @@
-import {TodoListComponent} from "./TodoListComponent";
-import {render, screen} from "@testing-library/react";
+import TodoListComponent from "./TodoListComponent";
+import { render, screen } from "@testing-library/react";
+import { store } from "../app/store";
+import { Provider } from "react-redux";
+import { initialState } from "../reducers/todo";
+
+const renderTodoList = () => {
+  return render(
+    <Provider store={store}>
+      <TodoListComponent />
+    </Provider>
+  );
+};
 
 describe("TodoListComponent", () => {
-    it("renders", () => {
-        render(<TodoListComponent />);
-        // eslint-disable-next-line testing-library/prefer-presence-queries
-        expect(screen.queryByTestId('todo-list-component')).toBeTruthy();
-    });
+  it("renders", () => {
+    renderTodoList();
+    // eslint-disable-next-line testing-library/prefer-presence-queries
+    expect(screen.queryByTestId("todo-list-component")).toBeTruthy();
+  });
 
-    it("will render out todos", () => {
-        const todos = [
-            {
-                id: 1,
-                text: "I am a todo"
-            },
-            {
-                id: 2,
-                text: "I am the second todo"
-            }
-        ];
-        render(<TodoListComponent todos={todos}/>);
+  it("will render out todos", () => {
+    renderTodoList();
 
-        todos.forEach((item) => {
-            // eslint-disable-next-line testing-library/prefer-presence-queries
-            expect(screen.queryByTestId(`todo-${item.id}`)).toBeTruthy();
-        })
+    initialState.todos.forEach((item) => {
+      // eslint-disable-next-line testing-library/prefer-presence-queries
+      expect(screen.queryByTestId(`todo-${item.id}`)).toBeTruthy();
     });
+  });
 });
